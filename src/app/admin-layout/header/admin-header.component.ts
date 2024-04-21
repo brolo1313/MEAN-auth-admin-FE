ed79d3bbf8dce9ab83da69011befdb2778e25667
Andrew Yupin <brolo1341@gmail.com>
2024-04-21 13:55:53 +0300

    added profile menu for user

--- src/app/admin-layout/header/admin-header.component.ts
+++ src/app/admin-layout/header/admin-header.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input, inject } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AuthService } from '../auth/services/auth.service';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { LocalStorageService } from '../auth/services/local-storage.services';
 
 @Component({
   selector: 'app-admin-header',
   standalone: true,
-  imports: [RouterModule, NgIf],
+  imports: [RouterModule, NgIf, MatMenuModule, MatDividerModule, MatIconModule],
   templateUrl: './admin-header.component.html',
   styleUrls: ['./admin-header.component.scss']
 })
@@ -17,12 +21,22 @@ export class AdminHeaderComponent {
 
   authService = inject(AuthService)
   router = inject(Router);
+  localStorage = inject(LocalStorageService);
 
-  public logOut(){
+  userSettings: any;
+
+  ngOnInit(): void {
+    const userSettingsString = this.localStorage.getUserSettings();
+    if (userSettingsString) {
+      this.userSettings = { ...JSON.parse(userSettingsString).userSettings };
+    }
+  }
+
+  public logOut() {
     this.authService.signOut();
   }
 
-  public pathToLogin(){
+  public pathToLogin() {
     this.router.navigate(['/login']);
   }
 }
