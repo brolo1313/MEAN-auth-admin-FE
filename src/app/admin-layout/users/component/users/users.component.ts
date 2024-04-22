4864cff59ddf609595c9c8b21b000fa0fdc4248a
Andrew Yupin <brolo1341@gmail.com>
2024-04-22 18:43:18 +0300

    improved UI/UX  users page

--- src/app/admin-layout/users/component/users/users.component.ts
+++ src/app/admin-layout/users/component/users/users.component.ts
@@ -2,13 +2,15 @@ import { UsersService } from '../../services/users.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { StoreUsersService } from '../../services/store-users.services';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contacs',
   standalone: true,
   imports: [
     CommonModule,
-    NgFor
+    NgFor,
+    MatIconModule
   ],
   styleUrl: './users.component.scss',
   templateUrl: './users.component.html',
